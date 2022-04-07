519a43d54d48cf486760bf9e0f52c3a47150a79d
Kirti Chachane <kbchachane@gmail.com>
2022-04-07 23:13:17 +0530

    created

---
+++ src/parcel/Delivery.java
@@ -0,0 +1,10 @@
+package parcel;
+
+public class Delivery {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("hello i m delivered");
+	}
+
+}
